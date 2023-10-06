732143d4b87b0799bc4450a6918c0ea2fcae01c5
Alex Loren <lorenalexm@gmail.com>
2023-10-06 15:29:19 -0700

    Removed macro to allow compilation on Windows

--- Sources/SwiftGodotCrossBuild/Entry.swift
+++ Sources/SwiftGodotCrossBuild/Entry.swift
@@ -8,4 +8,25 @@
 import SwiftGodot
 import SwiftGodotMacros
 
-#initSwiftExtension(cdecl: "swift_entry_point", types: [SpinningCube.self])
+//#initSwiftExtension(cdecl: "swift_entry_point", types: [SpinningCube.self])
+
+func setupScene(level: GDExtension.InitializationLevel) {
+	if level == .scene {
+		register(type: SpinningCube.self)
+	}
+}
+
+@_cdecl("swift_entry_point")
+public func swift_entry_point(
+	interfacePtr: OpaquePointer?,
+	libraryPtr: OpaquePointer?,
+	extensionPtr: OpaquePointer?
+) -> UInt8 {
+	print("Extension loaded.")
+	guard let interfacePtr, let libraryPtr, let extensionPtr else {
+		print("Error: Some parameters were not provided!")
+		return 0
+	}
+	initializeSwiftModule(interfacePtr, libraryPtr, extensionPtr, initHook: setupScene, deInitHook: { x in })
+	return 1
+}
