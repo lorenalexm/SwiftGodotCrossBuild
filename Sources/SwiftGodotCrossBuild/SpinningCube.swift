2f139fa5dee01196052f59d9335fbab4a725ce09
Alex Loren <lorenalexm@gmail.com>
2023-10-09 11:19:13 -0700

    Removed class attribute to support Windows

--- Sources/SwiftGodotCrossBuild/SpinningCube.swift
+++ Sources/SwiftGodotCrossBuild/SpinningCube.swift
@@ -7,8 +7,22 @@
 
 import SwiftGodot
 
-@Godot
 class SpinningCube: Node3D {
+	required init(nativeHandle _: UnsafeRawPointer) {
+		fatalError("init(nativeHandle:) called, it is a sign that something is wrong, as these objects should not be re-hydrated")
+	}
+
+	required init() {
+		SpinningCube._initClass
+		super.init()
+	}
+
+	static var _initClass: Void = {
+		let className = StringName("SpinningCube")
+		let classInfo = ClassInfo<SpinningCube>(name: className)
+	} ()
+
+
     public override func _ready() {
         let renderer = MeshInstance3D()
         renderer.mesh = BoxMesh()
