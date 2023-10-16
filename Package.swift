2e6b1e35a6b4101242419fcb56d70c3c1281c714
Alex Loren <lorenalexm@gmail.com>
2023-10-16 15:35:21 -0700

    Removed unneeded import

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import CompilerPluginSupport
 
 var linkerSettings: [LinkerSetting] = []
 #if os(macOS)
