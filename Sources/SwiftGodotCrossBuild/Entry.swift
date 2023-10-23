4158fde4c3aa0f94e3e3f87b4fe2177988d24d85
Alex Loren <lorenalexm@gmail.com>
2023-10-23 09:32:05 -0700

    Updated to reinstate macro usage

--- Sources/SwiftGodotCrossBuild/Entry.swift
+++ Sources/SwiftGodotCrossBuild/Entry.swift
@@ -7,23 +7,4 @@
 
 import SwiftGodot
 
-func setupScene(level: GDExtension.InitializationLevel) {
-	if level == .scene {
-		register(type: SpinningCube.self)
-	}
-}
-
-@_cdecl("swift_entry_point")
-public func swift_entry_point(
-	interfacePtr: OpaquePointer?,
-	libraryPtr: OpaquePointer?,
-	extensionPtr: OpaquePointer?
-) -> UInt8 {
-	print("Extension loaded.")
-	guard let interfacePtr, let libraryPtr, let extensionPtr else {
-		print("Error: Some parameters were not provided!")
-		return 0
-	}
-	initializeSwiftModule(interfacePtr, libraryPtr, extensionPtr, initHook: setupScene, deInitHook: { x in })
-	return 1
-}
+#initSwiftExtension(cdecl: "swift_entry_point", types: [SpinningCube.self])
