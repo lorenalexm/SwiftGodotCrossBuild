8239ee1328a6d9d00f476ec48b723175c905186b
Alex Loren <lorenalexm@gmail.com>
2023-10-24 05:56:40 -0700

    Experimental dependency to support Windows macros

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SwiftGodotCrossBuild"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/migueldeicaza/SwiftGodot", branch: "main"),
+        .package(url: "https://github.com/lorenalexm/SwiftGodot", branch: "windows-macros"),
     ],
     targets: [
         .target(
